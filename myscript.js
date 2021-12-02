67fa0a9ae4df8afb9c6b796eb15159ab2b120ef2
Alessio-Tomei <alessiotomei22@gmail.com>
2021-12-02 14:39:20 +0100

    Consegna finita

---
+++ myscript.js
@@ -0,0 +1,10 @@
+
+let userInputName = prompt('Inserisci il tuo nome');
+
+let userInputSurname = prompt('Inserisci il tuo cognome');
+
+let userInputFavoriteColor = prompt('Inserisci il tuo colore preferito');
+
+document.getElementById('password-generated').innerHTML = userInputName + userInputSurname + userInputFavoriteColor + 21;
+
+// document.getElementById('password-generated-2').innerHTML = prompt('Inserisci il tuo nome') + prompt('Inserisci il tuo cognnome') + prompt('Inserisci il tuo colore preferito') + 21;
